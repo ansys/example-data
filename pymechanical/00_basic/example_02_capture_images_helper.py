b6ac08295a4697fc1eb6e4879cc3823ffdcfe645
samigithub2022 <105867416+samigithub2022@users.noreply.github.com>
2023-02-26 22:20:45 -0500

    ExportImage works. ExportAnimation currently fails. Comment ExportAnimation. Enable PyMechanical example for ExportImage. (#115)

--- pymechanical/00_basic/example_02_capture_images_helper.py
+++ pymechanical/00_basic/example_02_capture_images_helper.py
@@ -116,11 +116,11 @@ for analysis in Model.Analyses:
         Graphics.ExportImage(pathvar + "_" + "Right" + ".png", GraphicsImageExportFormat.PNG, set2d)
 
 # Exporting a result animation to wmv video file with given resolution, frames and duration
-Graphics.ResultAnimationOptions.NumberOfFrames = 10
-Graphics.ResultAnimationOptions.Duration = Quantity(2, 's')
-settings = Ansys.Mechanical.Graphics.AnimationExportSettings(width = 1000, height = 665)
-
-result = Tree.FirstActiveObject
-pathvar = os.path.join(image_dir, result.Name)
-result.ExportAnimation(pathvar + ".wmv",GraphicsAnimationExportFormat.WMV,settings)
+# Graphics.ResultAnimationOptions.NumberOfFrames = 10
+# Graphics.ResultAnimationOptions.Duration = Quantity(2, 's')
+# settings = Ansys.Mechanical.Graphics.AnimationExportSettings(width = 1000, height = 665)
+#
+# result = Tree.FirstActiveObject
+# pathvar = os.path.join(image_dir, result.Name)
+# result.ExportAnimation(pathvar + ".wmv",GraphicsAnimationExportFormat.WMV,settings)
 
